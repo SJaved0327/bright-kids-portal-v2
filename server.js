3700b415335418d89807e5c0c933f10157ff7641
David Sosa <sosa413131@yahoo.com>
2018-01-24 19:17:39 -0500

    adding front-end React server along with back-end express server

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-parser");
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
+const router = express.Router();
 
 // Requiring our models for syncing
-const db = require("./models");
+// const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -15,9 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+
+//Use our router configuration when we call /api
+app.use('/api', router);
+
+//now  we can set the route path & initialize the API
+router.get('/', function(req, res) {
+  res.json({ message: 'API Initialized!'});
 });
+
+
+
+// Syncing our sequelize models and then starting our express app
+// db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+// });

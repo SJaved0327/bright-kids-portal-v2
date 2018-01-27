8203c765d628aaf0c1b61a49b6d950ffe9dfb49f
Alan Liang <ariez84@gmail.com>
2018-01-27 13:28:32 -0500

    react navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,2 +1,19 @@
 import React from "react";
 import "./navbar.css";
+
+const navBar = props => (
+<Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#home"><img src="/public/assets/logo.png" alt="bright-kids-logo" className="logo"></a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#">
+      Portal
+    </NavItem>
+    <NavItem eventKey={2} href="#">
+      Registration
+    </NavItem>
+  </Nav>
+</Navbar>;

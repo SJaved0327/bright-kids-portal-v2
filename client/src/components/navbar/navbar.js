98dc321d68460a99de90a103dd2142ac73b16b6f
Alan Liang <ariez84@gmail.com>
2018-01-30 00:08:11 -0500

    navbar working

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,26 +1,41 @@
 import React, {Component} from 'react'
-import "./navbar.css";
+import "./navbar.css"
+import { Navbar } from 'react-bootstrap'
+import { MenuItem } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import { NavItem } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
 
-class navbar extends Component {
+
+class NavbarTurtle extends Component {
     render() {
       return (
-    <div>
+<div>
         <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-            <a href="#home"><img src="/public/assets/logo.png" alt="bright-kids-logo" className="logo"></a>
-            </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-            <NavItem eventKey={1} href="#">
-            Portal
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-            Registration
-            </NavItem>
-        </Nav>
-        </Navbar>
-    </div>
-);
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#home">Bright-Kids</a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#">
+      Portal
+    </NavItem>
+    <NavItem eventKey={2} href="#">
+      Registration
+    </NavItem>
+    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Action</MenuItem>
+      <MenuItem eventKey={3.2}>Another action</MenuItem>
+      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+    </NavDropdown>
+  </Nav>
+</Navbar>
+</div>
+      )
+    }
+};
 
-export default navBar;
+export default NavbarTurtle;

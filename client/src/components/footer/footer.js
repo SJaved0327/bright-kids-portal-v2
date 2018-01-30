cee986df90b66e10059315f33ca46f5cc7004a51
David Sosa <DSosa@Davids-MacBook.fios-router.home>
2018-01-29 22:56:00 -0500

    added Fa icons to footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,24 +1,38 @@
 // import { Button } from 'react-bootstrap';
 import './footer.css'
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
+import FaPhone from 'react-icons/lib/fa/phone'
+import FaEnvelope from 'react-icons/lib/fa/envelope'
+import { Row, Col, Grid } from 'react-bootstrap';
 
 class footer extends Component {
-    render() {
-      return (
-        <div>
-          
+  render() {
+    return (
+      <div>
 
-<img src='./facebook.png' alt = 'facebook icon' height = '50px' width = '50px' className = 'footerButton'/>
-<img src='./youtube.png'  alt = 'youtube icon' height = '50px' width = '50px' className = 'footerButton'/>
-<img src='./twitter.png'  alt = 'twitter icon' height = '50px' width = '50px' className = 'footerButton'/>
-            
+        <Grid>
+          <Row className='footerRow'>
+            <Col xs={2}> 
+              <FaPhone className='faPhone'   size={30} />
+              <br/>
+              <span className = 'footerPhone footerText'>(646) 434-1084 </span>
+            </Col>
+            <Col xs={8}>
+              <img src='./facebook.png' alt='facebook icon' height='50px' width='50px' className='footerButton' />
+              <img src='./youtube.png' alt='youtube icon' height='50px' width='50px' className='footerButton' />
+              <img src='./twitter.png' alt='twitter icon' height='50px' width='50px' className='footerButton' />
+            </Col>
 
-
-        </div>
-      );
-    }
+            <Col xs={2}>
+              <FaEnvelope className='faEnvelope'  size={30} />
+              <br/>
+              <span className = 'footerEmail footerText'>info@brightkidsnyc.com</span>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
-  
-  export default footer;
-  
+}
+
+export default footer;

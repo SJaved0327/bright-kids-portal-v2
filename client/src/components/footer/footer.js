6da279f6d7e7c8e2282ce35f246e13663391f34f
David Sosa <DSosa@Davids-MacBook.local>
2018-01-30 12:36:19 -0500

    added more detail to footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -11,22 +11,30 @@ class footer extends Component {
       <div>
 
         <Grid>
+          <hr className = 'footerHr'/>
           <Row className='footerRow'>
             <Col xs={2}> 
-              <FaPhone className='faPhone'   size={30} />
-              <br/>
+
               <span className = 'footerPhone footerText'>(646) 434-1084 </span>
+  <br/>
+              <FaPhone className='faPhone'   size={30} />
+
             </Col>
             <Col xs={8}>
+            Follow us on social media!
+
+            <div className ='allFooterButtons'>
               <img src='./facebook.png' alt='facebook icon' height='50px' width='50px' className='footerButton' />
               <img src='./youtube.png' alt='youtube icon' height='50px' width='50px' className='footerButton' />
               <img src='./twitter.png' alt='twitter icon' height='50px' width='50px' className='footerButton' />
+              </div>
             </Col>
 
             <Col xs={2}>
+            <span className = 'footerEmail footerText'>info@brightkidsnyc.com</span>
               <FaEnvelope className='faEnvelope'  size={30} />
-              <br/>
-              <span className = 'footerEmail footerText'>info@brightkidsnyc.com</span>
+
+
             </Col>
           </Row>
         </Grid>

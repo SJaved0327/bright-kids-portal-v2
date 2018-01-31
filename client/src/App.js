437b3600651f19fe9745cb4cab3073830926814e
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2018-01-31 12:57:52 -0500

    exported NavbarTurtle to App.js. Now displays.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Footer from './components/footer';
-import Navbar from './components/navbar';
+import NavbarTurtle from './components/navbar';
 import SignIn from './components/signin';
 
 
@@ -11,16 +11,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        <NavbarTurtle />
         <SignIn />
         <Footer/>
+        
       </div>
     )
   }

c77b235c9b1838f6e6daeb16ee9db4b26409acef
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2018-01-31 19:48:16 -0500

    footer links working and open into new windows

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -24,10 +24,20 @@ class footer extends Component {
             Follow us on social media!
 
             <div className ='allFooterButtons'>
-              <img src='./facebook.png' alt='facebook icon' height='50px' width='50px' className='footerButton' />
-              <img src='./youtube.png' alt='youtube icon' height='50px' width='50px' className='footerButton' />
-              <img src='./twitter.png' alt='twitter icon' height='50px' width='50px' className='footerButton' />
-              </div>
+              {/*Facebook*/}
+              <a href="https://www.facebook.com/brightkidsinc/" target="_blank">
+                <img src='./facebook.png' alt='facebook icon' height='50px' width='50px' className='footerButton' />
+              </a>
+              {/*YoutTube*/}
+              <a href="https://www.youtube.com/channel/UCrA-x6F7l3bg--v0NmDuw2Q" target="_blank">
+               <img src='./youtube.png' alt='youtube icon' height='50px' width='50px' className='footerButton' />
+              </a>
+            {/*Twitter*/}
+              <a href="https://twitter.com/BrightKidsInc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank">
+                <img src='./twitter.png' alt='twitter icon' height='50px' width='50px' className='footerButton' />
+              </a>
+            </div>
+
             </Col>
 
             <Col xs={2}>

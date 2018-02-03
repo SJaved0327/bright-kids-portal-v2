7faf578e2a5033626cd57cef51812f2160900d1e
David W Sosa <sosa413131@yahoo.com>
2018-02-03 11:53:26 -0500

    fixed conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,7 @@ import NavbarTurtle from './components/navbar';
 // import LogIn from './pages/LogIn';
 // import NoMatch from './pages/NoMatch';
 // import Select from './pages/Select';
-<<<<<<< HEAD
-// import ViewFamily from './components/pages/ViewFamily';
-
-=======
 import select_path from './pages/select_path'
->>>>>>> b5bf08b2b1a83fd70004ea1c52ee331927973e5d
 
 const App = () =>
 	<Router>
@@ -28,17 +23,9 @@ const App = () =>
 			<Switch>
 				{/* <Route exact path="/" component={LogIn} />
 				<Route exact path="/select" component={Select} />
-<<<<<<< HEAD
-				<Route exact path="/family" component={ViewFamily} />
-				<Route component={NoMatch} />
-
-			</Switch> */}
-			
-=======
 				<Route component={NoMatch} /> */}
 				<Route exact path = "/select_path" component={select_path}/>
 			</Switch>
->>>>>>> b5bf08b2b1a83fd70004ea1c52ee331927973e5d
 			<Footer />
 		</div>
 	</Router>;

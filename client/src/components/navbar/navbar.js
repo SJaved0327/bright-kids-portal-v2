4557d1b61d3b2513b2fc3fa2efc8db36d9317187
Alan Liang <ariez84@gmail.com>
2018-02-04 00:36:42 -0500

    testing logo

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -14,7 +14,7 @@ class NavbarTurtle extends Component {
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#home">Bright-Kids</a>
+              <a href="#home"><img src="./logo.png" width="150px" height="75px" alt="bright kids logo" /></a>
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>

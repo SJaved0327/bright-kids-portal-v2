25d406bc820abd160b2c411ae1da3b300879ec32
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2018-02-12 20:13:34 -0500

    added dummy evaluations data

--- client/src/pages/StudentEvaluations/StudentEvaluations.js
+++ client/src/pages/StudentEvaluations/StudentEvaluations.js
@@ -17,8 +17,62 @@ import RightPageArrow from '../../components/RightPageArrow'
 
 // assets
 
-const StudentEvaluations = () => (
-	<h3>StudentEvaluations</h3>
-);
+class StudentEvaluations extends React.Component {
+	
+	state = {
+		evaluations: [
+			{
+				date: "01/01/2018",
+				test_type: "Gifted & Talented",
+				results: {
+					subtests: [
+					{ 
+						OLSAT: {
+							Following_Directions : {
+								correct: 7, 
+								total: 10
+							},
+							Arithmetic_Reasoning : {
+								correct: 4, 
+								total: 10
+							},
+							Aural_Reasoning : {
+								correct: 5, 
+								total: 10
+							}
+						},{
+						NNAT: {
+							Pattern_Completion : {
+								correct: 3, 
+								total: 10
+							},
+							Reasoning_By_Analogy: {
+								correct: 2, 
+								total: 10
+							},
+							Serial_Reasoning : {
+								correct: 3, 
+								total: 10
+							},
+							Spatial_Visualization : {
+								correct: 3, 
+								total: 10
+							}
+					}
+				],
+					notes: "It was a pleasure working with John today."
+				}
+			}
+		]
+	}
+
+
+	render(){
+		return(
+		<h3>StudentEvaluations</h3>
+	)
+	}	
+}
+
 
 export default StudentEvaluations;

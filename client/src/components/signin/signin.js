cfed33509d7f7e14b1bd3f639c9501902a32b73f
Sara Javed <sara.joann.javed@gmail.com>
2018-10-28 17:44:01 -0400

    created test parameter for login; if correct credentials entered, user is sent to select_path page

--- client/src/components/signin/signin.js
+++ client/src/components/signin/signin.js
@@ -20,77 +20,70 @@ const style = {
 
 class SignIn extends React.Component {
 
-
 	constructor(props) {
 		super(props);
-
 		// initial state in forms set to blank
 		this.state = {
 			emailInput: "",
 			passwordInput:  ""
 		};
-
 		// This binding is necessary to make `this` work in the callback
 		this.handleSubmit = this.handleSubmit.bind(this);
-
 		this.handleEmailChange = this.handleEmailChange.bind(this);
-
 		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+	};
 
-	}
-
-
+	//whatever the user types into the email field will update the state
 	handleEmailChange(event) {
 		this.setState({
 			emailInput: event.target.value
 		});
-	}
+	};
 
+	//whatever the user types into the password field will update the state
 	handlePasswordChange(event) {
 		this.setState({
 			passwordInput: event.target.value
 		});
-	}
+	};
 
 	handleSubmit(event) {
 		event.preventDefault();
-		// console.log("Here is THIS",this)
-		// console.log("Here is submit",event.target.submit.value)
-
-		// var email = event.target.emailInput.value;
-		// var password = event.target.passwordInput.value;
-
-		var email = this.state.emailInput;
-		var password = this.state.passwordInput;
-
-
+		// variables will be set to the values of current state
+		let email = this.state.emailInput;
+		let password = this.state.passwordInput;
+		// print email and password values to console
 		console.log("email:", email, typeof email)
 		console.log("password:", password, typeof password)
 
+		//if input matches test email + password requirements, load select_path page
+		if ((email === "test@test.com") && (password === "pw123")){
+			console.log("approved");
+			window.location.href = '/select_path';
+		}else{
+			alert("Error. Try again.")
+		}
 
-		function logInAttempt(query) {
-			return axios.post(window.location.href + "auth/signin", {
+		// function logInAttempt(query) {
+		// 	return axios.post(window.location.href + "auth/signin", {
 
-				"email": email,
-				"password": password
+		// 		"email": email,
+		// 		"password": password
 
-			}).then(function (response) {
-				console.log("response received!")
-				console.log(response);
-				if (response.data.signedIn == "true") {
-					console.log("this should be rerouting now")
-					window.location.href = window.location.href + 'select_path'
-				} else{
-					window.location.href = window.location.href
-				}
+		// 	}).then(function (response) {
+		// 		console.log("response received!")
+		// 		console.log(response);
+		// 		if (response.data.signedIn == "true") {
+		// 			console.log("this should be rerouting now")
+		// 			window.location.href = window.location.href + 'select_path'
+		// 		} else{
+		// 			window.location.href = window.location.href
+		// 		}
 
-			})
-
-		} return logInAttempt()
+		// 	})
+		// } return logInAttempt()
 
 	}
-
-
 
 	render() {
 		return (
